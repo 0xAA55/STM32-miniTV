bc3547682498fd907660d7bcc5e78e3013d4f609
0xAA55 <weixiaobao213@qq.com>
2025-12-09 14:17:22 +0800

    增加一种新的绘制几何图形的方法：水平线方法。每个水平线结构体存储当前 y 偏移量，x 偏移量，以及水平线长度。

--- Core/Inc/graphics.h
+++ Core/Inc/graphics.h
@@ -23,5 +23,15 @@ Pixel565 ColorFromPhase(uint32_t phase, uint32_t brightness);
 void DrawText(int x, int y, const char* text, Pixel565 TextColor);
 void DrawTextOpaque(int x, int y, const char* text, Pixel565 TextColor, Pixel565 BgColor);
 
+typedef struct hls
+{
+  int8_t y_offset;
+  int8_t x_offset;
+  int8_t x_length;
+}HorzLine;
+
+// Max scaling is 512
+void DrawHorzLines(int x_center, int y_center, const HorzLine *lines, size_t count, Pixel565 color, int scaling);
+void DrawPlayButton(int x_center, int y_center, Pixel565 c1, Pixel565 c2, int scaling);
 
 #endif /* INC_GRAPHICS_H_ */
