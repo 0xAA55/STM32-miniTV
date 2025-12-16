cb8d47e8f2f8f665b0c929720616538168f18ed2
0xAA55 <weixiaobao213@qq.com>
2025-12-16 22:47:07 +0800

    因为要用 FatFs，需要处理 UTF-16 字符串，所以新增 UTF-16 到 Unicode 的互转的库。

---
+++ Core/Inc/utf16.h
@@ -0,0 +1,27 @@
+/*
+ * utf16.h
+ *
+ *  Created on: 2025年12月16日
+ *      Author: 0xaa55
+ */
+
+#ifndef INC_UTF16_H_
+#define INC_UTF16_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct UTF16Parser_s
+{
+  const uint16_t *ptr;
+  size_t rem;
+}UTF16Parser;
+
+size_t strlenW(const uint16_t *utf16);
+uint16_t * strcpyW(uint16_t *destination, const uint16_t *source);
+UTF16Parser utf16_start_parse(const uint16_t *utf16);
+uint32_t utf16_to_utf32(UTF16Parser *p, uint32_t bad_char_code);
+void utf16_end_parse(UTF16Parser *p);
+uint16_t *utf32_to_utf16(uint32_t code, uint16_t *utf16);
+
+#endif /* INC_UTF16_H_ */
---
+++ Core/Src/utf16.c
@@ -0,0 +1,106 @@
+/*
+ * utf16.c
+ *
+ *  Created on: 2025年12月16日
+ *      Author: 0xaa55
+ */
+
+#include "utf16.h"
+#include <string.h>
+
+#define UNUSED(x) ((void)(x))
+
+size_t strlenW(const uint16_t *utf16)
+{
+  size_t ret = 0;
+  while(*utf16++) ret++;
+  return ret;
+}
+
+uint16_t * strcpyW(uint16_t *destination, const uint16_t *source)
+{
+  size_t len = strlenW(source) + 1;
+  memcpy(destination, source, len * 2);
+  return destination;
+}
+
+UTF16Parser utf16_start_parse(const uint16_t *utf16)
+{
+  UTF16Parser ret =
+  {
+    utf16,
+    strlenW(utf16),
+  };
+  return ret;
+}
+
+static uint32_t process_surrogate(uint16_t c1, uint16_t c2)
+{
+  uint32_t h = c1 & 0x3FF;
+  uint32_t l = c2 & 0x3FF;
+  return ((h << 10) | l) + 0x10000;
+}
+
+uint32_t utf16_to_utf32(UTF16Parser *p, uint32_t bad_char_code)
+{
+  const uint16_t *utf16s = p->ptr;
+  if (!p->rem) return 0;
+  switch (utf16s[0] & 0xFC00)
+  {
+    default:
+      p->ptr ++;
+      p->rem --;
+      return utf16s[0];
+    case 0xD800:
+      switch (utf16s[1] & 0xFC00)
+      {
+        default:
+          return bad_char_code;
+        case 0xDC00:
+          if (p->rem < 2)
+          {
+            p->ptr ++;
+            p->rem --;
+            return bad_char_code;
+          }
+          p->ptr += 2;
+          p->rem -= 2;
+          return process_surrogate(utf16s[0], utf16s[1]);
+      }
+    case 0xDC00:
+      switch (utf16s[1] & 0xFC00)
+      {
+        default:
+          return bad_char_code;
+        case 0xD800:
+          if (p->rem < 2)
+          {
+            p->ptr ++;
+            p->rem --;
+            return bad_char_code;
+          }
+          p->ptr += 2;
+          p->rem -= 2;
+          return process_surrogate(utf16s[1], utf16s[0]);
+      }
+  }
+}
+
+void utf16_end_parse(UTF16Parser *p)
+{
+  UNUSED(p);
+}
+
+uint16_t *utf32_to_utf16(uint32_t code, uint16_t *utf16)
+{
+  if (code < 0x10000)
+    *utf16++ = code;
+  else
+  {
+    code -= 0x10000;
+    *utf16++ = 0xD800 | ((code >> 10) & 0x3FF);
+    *utf16++ = 0xDC00 | (code & 0x3FF);
+  }
+  return utf16;
+}
+
