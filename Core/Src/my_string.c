1181568b5d7aa46db65df7fc4cfb4d8656a16345
0xAA55 <weixiaobao213@qq.com>
2025-12-17 13:01:16 +0800

    因为做了对齐检测了，所以启用它

--- Core/Src/my_string.c
+++ Core/Src/my_string.c
@@ -14,7 +14,6 @@
 
 #pragma GCC optimize ("no-tree-loop-distribute-patterns")
 
-/*
 void *memset(void * dst, int val, size_t len)
 {
   uint32_t *ptr_dst = dst;
@@ -54,7 +53,6 @@ void *memset(void * dst, int val, size_t len)
   }
   return dst;
 }
-*/
 
 void *memcpy(void * dst, const void * src, size_t len)
 {
