c902c24689076e64393353929321a0a24e3c21c8
0xAA55 <weixiaobao213@qq.com>
2025-12-17 10:22:04 +0800

    修复 `target_menu` 没被先初始化的问题；修复 `GUICurMenu` 没被先限制的问题。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -192,14 +192,14 @@ void UpdateEnc1MainMenuState(int delta_tick, int enc_delta)
   int target_menu;
   int menu_speed = 5 * delta_tick;
   enc_delta = imax(-1, imin(enc_delta, 1));
-  if (GUICurMenu < 0) GUICurMenu = 0;
-  if (GUICurMenu > 3) GUICurMenu = 3;
+  target_menu = GUICurMenu * 1024;
   if (abs(GUIMenuAnim - target_menu) <= 768)
   {
     GUIMenuReady = 0;
     GUICurMenu += enc_delta;
   }
-  target_menu = GUICurMenu * 1024;
+  if (GUICurMenu < 0) GUICurMenu = 0;
+  if (GUICurMenu > 3) GUICurMenu = 3;
   if (GUIMenuAnim < target_menu)
   {
     if (GUIMenuAnim + menu_speed >= target_menu)
