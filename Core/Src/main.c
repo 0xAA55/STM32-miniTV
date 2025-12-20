9bf1d9bbf80ae3254c03ae73268b720f1b7ba2d6
0xAA55 <weixiaobao213@qq.com>
2025-12-20 21:09:49 +0800

    启用硬件控制流，启用 IDMA，将 `FATFS FatFs` 放入 AXI SRAM，解决了无法读写 SD 卡的问题。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -53,7 +53,6 @@ __attribute__((section(".dtcm_bss"))) extern SPI_HandleTypeDef hspi1;
 __attribute__((section(".dtcm_bss"))) extern DMA_HandleTypeDef hdma_spi1_tx;
 __attribute__((section(".dtcm_bss"))) extern DMA_HandleTypeDef hdma_spi1_rx;
 __attribute__((section(".dtcm_bss"))) extern PCD_HandleTypeDef hpcd_USB_OTG_HS;
-__attribute__((section(".dtcm_bss"))) extern FATFS FatFs;
 __attribute__((section(".dtcm_bss"))) extern LCD hlcd;
 __attribute__((section(".dtcm_data"))) extern Pixel565 (*CurDrawFramebuffer)[320];
 /* USER CODE END PD */
@@ -938,7 +937,7 @@ static void MX_SDMMC1_SD_Init(void)
   hsd1.Init.ClockEdge = SDMMC_CLOCK_EDGE_RISING;
   hsd1.Init.ClockPowerSave = SDMMC_CLOCK_POWER_SAVE_DISABLE;
   hsd1.Init.BusWide = SDMMC_BUS_WIDE_4B;
-  hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_DISABLE;
+  hsd1.Init.HardwareFlowControl = SDMMC_HARDWARE_FLOW_CONTROL_ENABLE;
   hsd1.Init.ClockDiv = 2;
   /* USER CODE BEGIN SDMMC1_Init 2 */
   HAL_SD_MspInit(&hsd1);
