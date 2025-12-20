3a813403ac09cecf2f71f50c1af6f088d08a1187
0xAA55 <weixiaobao213@qq.com>
2025-12-20 19:36:25 +0800

    去除 SD 卡测试代码。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -137,8 +137,6 @@ __attribute__((section(".dtcm_bss"))) volatile int BAT_ADC_Sampling;
 __attribute__((section(".dtcm_bss"))) volatile int MainBtnClick;
 __attribute__((section(".dtcm_bss"))) volatile int SecondBtnClick;
 __attribute__((section(".dtcm_bss"))) volatile int BAT_Voltage;
-__attribute__((section(".dtcm_bss"))) volatile int SD_Tx_Cplt;
-__attribute__((section(".dtcm_bss"))) volatile int SD_Rx_Cplt;
 __attribute__((section(".itcm_code")))
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
@@ -349,30 +347,6 @@ void GetCurDirFileList()
     f_closedir(&dir);
   }
 }
-__attribute__((section(".itcm_code")))
-void BSP_SD_ReadCpltCallback(void)
-{
-  SD_Rx_Cplt = 1;
-}
-void WaitForSDRXCplt(uint32_t timeout)
-{
-  uint32_t until = HAL_GetTick() + timeout;
-  while(!SD_Rx_Cplt && HAL_GetTick() < until) __WFI();
-  SD_Rx_Cplt = 0;
-}
-HAL_StatusTypeDef TestSDRead()
-{
-  HAL_StatusTypeDef ret;
-  ret = HAL_SD_Init(&hsd1);
-  if (ret != HAL_OK) return ret;
-  static uint8_t sd_buffer[512];
-  ret = HAL_SD_ReadBlocks_IT(&hsd1, sd_buffer, 0, 1);
-  if (ret != HAL_OK) return ret;
-  WaitForSDRXCplt(200);
-  ret = HAL_ERROR;
-  if (sd_buffer[510] == 0x55 && sd_buffer[511] == 0xAA) ret = HAL_OK;
-  HAL_SD_DeInit(&hsd1);
-}
 /* USER CODE END 0 */
 
 /**
@@ -458,7 +432,6 @@ int main(void)
   UpdatePowerRead();
   WaitForPresent();
   HAL_GPIO_WritePin(LCD_PWCTRL_GPIO_Port, LCD_PWCTRL_Pin, GPIO_PIN_SET);
-  TestSDRead();
   /* USER CODE END 2 */
 
   /* Infinite loop */
