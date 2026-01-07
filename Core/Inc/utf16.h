a60d660fbb04e9cada11f1fb8e928a5b9f66736c
0xAA55 <weixiaobao213@qq.com>
2026-01-08 00:52:26 +0800

    实现 `strcatW()`

--- Core/Inc/utf16.h
+++ Core/Inc/utf16.h
@@ -18,9 +18,10 @@ typedef struct UTF16Parser_s
 }UTF16Parser;
 
 size_t strlenW(const uint16_t *utf16);
-uint16_t * strcpyW(uint16_t *destination, const uint16_t *source);
-uint16_t * strncpyW(uint16_t *destination, const uint16_t *source, size_t maxlen);
+uint16_t *strcpyW(uint16_t *destination, const uint16_t *source);
+uint16_t *strncpyW(uint16_t *destination, const uint16_t *source, size_t maxlen);
 int strcmpW(const uint16_t *str1, const uint16_t *str2);
+uint16_t *strcatW(uint16_t *destination, const uint16_t *source);
 UTF16Parser utf16_start_parse(const uint16_t *utf16);
 uint32_t utf16_to_utf32(UTF16Parser *p, uint32_t bad_char_code);
 void utf16_end_parse(UTF16Parser *p);
