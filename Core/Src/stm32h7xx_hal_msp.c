6dff9bce66f48294de4f8921f069a5a3e90ea319
0xAA55 <weixiaobao213@qq.com>
2026-01-12 00:51:42 +0800

    调整 MDMA 的参数，使其尽可能降低总线占用。

--- Core/Src/stm32h7xx_hal_msp.c
+++ Core/Src/stm32h7xx_hal_msp.c
@@ -362,16 +362,16 @@ void HAL_JPEG_MspInit(JPEG_HandleTypeDef* hjpeg)
     hmdma_jpeg_infifo_th.Instance = MDMA_Channel0;
     hmdma_jpeg_infifo_th.Init.Request = MDMA_REQUEST_JPEG_INFIFO_TH;
     hmdma_jpeg_infifo_th.Init.TransferTriggerMode = MDMA_BUFFER_TRANSFER;
-    hmdma_jpeg_infifo_th.Init.Priority = MDMA_PRIORITY_LOW;
+    hmdma_jpeg_infifo_th.Init.Priority = MDMA_PRIORITY_HIGH;
     hmdma_jpeg_infifo_th.Init.Endianness = MDMA_LITTLE_ENDIANNESS_PRESERVE;
-    hmdma_jpeg_infifo_th.Init.SourceInc = MDMA_SRC_INC_BYTE;
+    hmdma_jpeg_infifo_th.Init.SourceInc = MDMA_SRC_INC_WORD;
     hmdma_jpeg_infifo_th.Init.DestinationInc = MDMA_DEST_INC_DISABLE;
-    hmdma_jpeg_infifo_th.Init.SourceDataSize = MDMA_SRC_DATASIZE_BYTE;
+    hmdma_jpeg_infifo_th.Init.SourceDataSize = MDMA_SRC_DATASIZE_WORD;
     hmdma_jpeg_infifo_th.Init.DestDataSize = MDMA_DEST_DATASIZE_WORD;
     hmdma_jpeg_infifo_th.Init.DataAlignment = MDMA_DATAALIGN_PACKENABLE;
     hmdma_jpeg_infifo_th.Init.BufferTransferLength = 32;
-    hmdma_jpeg_infifo_th.Init.SourceBurst = MDMA_SOURCE_BURST_SINGLE;
-    hmdma_jpeg_infifo_th.Init.DestBurst = MDMA_DEST_BURST_SINGLE;
+    hmdma_jpeg_infifo_th.Init.SourceBurst = MDMA_SOURCE_BURST_32BEATS;
+    hmdma_jpeg_infifo_th.Init.DestBurst = MDMA_DEST_BURST_32BEATS;
     hmdma_jpeg_infifo_th.Init.SourceBlockAddressOffset = 0;
     hmdma_jpeg_infifo_th.Init.DestBlockAddressOffset = 0;
     if (HAL_MDMA_Init(&hmdma_jpeg_infifo_th) != HAL_OK)
@@ -388,18 +388,18 @@ void HAL_JPEG_MspInit(JPEG_HandleTypeDef* hjpeg)
 
     /* JPEG_OUTFIFO_NE Init */
     hmdma_jpeg_outfifo_ne.Instance = MDMA_Channel1;
-    hmdma_jpeg_outfifo_ne.Init.Request = MDMA_REQUEST_JPEG_OUTFIFO_NE;
+    hmdma_jpeg_outfifo_ne.Init.Request = MDMA_REQUEST_JPEG_OUTFIFO_TH;
     hmdma_jpeg_outfifo_ne.Init.TransferTriggerMode = MDMA_BUFFER_TRANSFER;
-    hmdma_jpeg_outfifo_ne.Init.Priority = MDMA_PRIORITY_LOW;
+    hmdma_jpeg_outfifo_ne.Init.Priority = MDMA_PRIORITY_VERY_HIGH;
     hmdma_jpeg_outfifo_ne.Init.Endianness = MDMA_LITTLE_ENDIANNESS_PRESERVE;
     hmdma_jpeg_outfifo_ne.Init.SourceInc = MDMA_SRC_INC_DISABLE;
-    hmdma_jpeg_outfifo_ne.Init.DestinationInc = MDMA_DEST_INC_BYTE;
+    hmdma_jpeg_outfifo_ne.Init.DestinationInc = MDMA_DEST_INC_WORD;
     hmdma_jpeg_outfifo_ne.Init.SourceDataSize = MDMA_SRC_DATASIZE_WORD;
-    hmdma_jpeg_outfifo_ne.Init.DestDataSize = MDMA_DEST_DATASIZE_BYTE;
+    hmdma_jpeg_outfifo_ne.Init.DestDataSize = MDMA_DEST_DATASIZE_WORD;
     hmdma_jpeg_outfifo_ne.Init.DataAlignment = MDMA_DATAALIGN_PACKENABLE;
-    hmdma_jpeg_outfifo_ne.Init.BufferTransferLength = 4;
-    hmdma_jpeg_outfifo_ne.Init.SourceBurst = MDMA_SOURCE_BURST_SINGLE;
-    hmdma_jpeg_outfifo_ne.Init.DestBurst = MDMA_DEST_BURST_SINGLE;
+    hmdma_jpeg_outfifo_ne.Init.BufferTransferLength = 32;
+    hmdma_jpeg_outfifo_ne.Init.SourceBurst = MDMA_SOURCE_BURST_32BEATS;
+    hmdma_jpeg_outfifo_ne.Init.DestBurst = MDMA_DEST_BURST_32BEATS;
     hmdma_jpeg_outfifo_ne.Init.SourceBlockAddressOffset = 0;
     hmdma_jpeg_outfifo_ne.Init.DestBlockAddressOffset = 0;
     if (HAL_MDMA_Init(&hmdma_jpeg_outfifo_ne) != HAL_OK)
