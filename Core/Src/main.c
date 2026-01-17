77cc70b332a4fb052c90192b05ca747cef16d6f1
0xAA55 <weixiaobao213@qq.com>
2026-01-17 21:53:49 +0800

    增加对 ROM 文件的版本的检测。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1156,13 +1156,6 @@ static void PrepareBugFile()
     ShowNotify(1000, "加载文件失败");
     goto FailExit;
   }
-  if (ptr->Signature == 0xAA55 && size <= 8 * 1024 * 1024)
-  {
-    BugFileAgreed = 0;
-    GUIIsUsingFile = 1;
-    return;
-  }
-  /*
   if (ptr->Signature == 0xAA55 && !(ptr->Version & 0xFFF00000) && size <= 8 * 1024 * 1024)
   {
     if (ptr->Version < FLASH_MAP->Version)
@@ -1174,7 +1167,6 @@ static void PrepareBugFile()
     GUIIsUsingFile = 1;
     return;
   }
-  */
   ShowNotify(1000, "不是固件升级文件 %x %x %u", ptr->Signature, ptr->Version, size);
 FailExit:
   QuitBugFile();
