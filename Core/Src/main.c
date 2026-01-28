be03fce4a90c816d7cd7d4ee2470ad57682b4961
0xAA55 <weixiaobao213@qq.com>
2026-01-28 18:26:18 +0800

    「设置」界面的抬头设置为蓝色。

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1964,7 +1964,7 @@ void OnOptionsGUI(uint64_t cur_tick, int delta_tick, int enc1_delta, int enc1_cl
 {
   ClearScreen(MakePixel565(0, 0, 0));
 
-  FillRect(0, 0, 320, 20, MakePixel565(255, 255, 255));
+  FillRect(0, 0, 320, 18, MakePixel565(102, 204, 255));
   DrawText(140, 0, 180, 20, "设置", MakePixel565(0, 0, 0));
   DrawText(0, 20, 320, 240, "设置字幕字体大小\n设置快进、快退步长\n设置待机时间\n管理文件\n本机信息", MakePixel565(255, 255, 255));
 
