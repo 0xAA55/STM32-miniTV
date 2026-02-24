3bcdcdb03b1f28a859f1ee99a3c2476b3955f269
0xAA55 <weixiaobao213@qq.com>
2026-02-24 19:01:50 +0800

    将缓冲区改的更大。

--- USB_DEVICE/Target/usbd_conf.h
+++ USB_DEVICE/Target/usbd_conf.h
@@ -75,7 +75,7 @@
 /*---------- -----------*/
 #define USBD_SELF_POWERED     1U
 /*---------- -----------*/
-#define MSC_MEDIA_PACKET     4096U
+#define MSC_MEDIA_PACKET     32768U
 
 /****************************************/
 /* #define for FS and HS identification */
